ee464c953bf1d79a4a293e1e844bde01cd3e3aac
“JyothiThipplikat” <lilmuthu4life21@gmail.com>
2018-11-01 21:26:30 -0500

    bootstrap and display student 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,59 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="top"></div>
+
+       <!-- icon-nav-wrapper -->
+    <div id="icon-nav-wrapper" class="side-nav-inverse">
+      
+      <!-- side navigation -->
+        <a href="#menu-toggle" id="menu-toggle"><i class="fa fa-bars"></i></a>
+        <div id="icon-nav-sidebar-wrapper" class="nav-inverse">
+            <ul id="nav-scroll" class="sidebar-nav sidebar-nav-inverse">
+                <li class="sidebar-brand">
+                    <a href="#top" class="smooth-scroll"><img src="images/icon-nav-logo.png" alt=""></a>
+                </li>
+                <li>
+                    <a class="menu-item smooth-scroll nav-link active" href="#top"><i class="md-icon dp24">home</i></a>
+                </li>
+                <li>
+                    <a class="menu-item smooth-scroll nav-link" href="#about"><i class="md-icon dp24">person</i></a>
+                </li>
+                <li>
+                    <a class="menu-item smooth-scroll nav-link" href="#education"><i class="md-icon dp24">school</i></a>
+                </li>
+                <li>
+                    <a class="menu-item smooth-scroll nav-link" href="#experience"><i class="md-icon dp24">work</i></a>
+                </li>
+                <li>
+                    <a class="menu-item smooth-scroll nav-link" href="#work"><i class="md-icon dp24">apps</i></a>
+                </li>
+                <li>
+                    <a class="menu-item smooth-scroll nav-link" href="#contact"><i class="md-icon dp24">email</i></a>
+                </li>
+            </ul>
+        </div>
+      <!-- / side navigation -->
+
     </div>
+
     <router-view/>
+
+    <footer class="dark bg-inverse">
+      <div class="container-fluid text-center">
+        <div class="row">
+          <div class="col-lg-8 footer-left-area">
+            <p>Copyright © 2017. BASIX by <a href="http://kingstudio.ro" target="_blank">KingStudio</a>. All Rights Reserved.</p>
+          </div>
+          <div class="col-lg-4 social-icons footer-right-area">
+            <a href="#x"><i class="fa fa-facebook"></i></a>
+            <a href="#x"><i class="fa fa-twitter"></i></a>
+            <a href="#x"><i class="fa fa-instagram"></i></a>
+            <a href="#x"><i class="fa fa-dribbble"></i></a>
+          </div><!-- / social-icons -->
+        </div><!-- / row -->
+      </div><!-- / container-fluid -->
+  </footer>
+
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,77 @@
 <template>
-  <div class="home">
-    <div v-for="student in students">{{ student }}</div>  
-  </div>
+<div class="student-show">
+    <header class="resume-demo-dark w-profile-image bg-img dark">
+                <div class="container-fluid header-inner">
+                    <div class="row vcenter">
+                        <div class="col-md-4">
+                            <img src="images/johndoe.jpg" alt="" class="circle inner-space-2x">
+                        </div><!-- / column -->
+                        <div class="col-md-8">
+                            <h1 class="header-title"><span class="text-primary">HELLO!</span> I'M {{student.first}} {{student.last}}</h1>
+                            <h5 class="mt-2 mb-3 opc-75">{{student.email}} - {{student.phone}}</h5>
+                            <p class="lead">{{student.bio}}</p>
+                            <a href="#about" class="btn btn-primary smooth-scroll mt-3"><span class="text-black">MORE ABOUT ME</span> <i class="md-icon dp14 text-black">expand_more</i></a>
+                        </div><!-- / column -->
+                    </div><!-- / row -->
+                </div><!-- / container-fluid -->
+            </header> 
+
+
+
+
+
+
+  <p>{{student.email}}</p>
+  <h2>{{student.phone}}</h2>
+  <!-- <h2>{{student.education.start_date}}</h2>
+  <h2>{{student.education.end_date}}</h2>
+  <h2>{{student.eduation.university_name}}</h2>
+  <h2>{{student.education.degree}}</h2>
+  <h2>{{student.education.details}}</h2>
+  <h2>{{student.experience.start_time}}</h2>
+  <h2>{{student.experience.end_time}}</h2>
+  <h2>{{student.experience.job_title}}</h2>
+  <h2>{{student.experience.details}}</h2>
+  <<h2>{{student.skill.skill_name}}</h2> -->
+
+
+
+
+
+  <div class="cta bg-primary">
+    <div class="container-fluid">
+      <div class="row vcenter">
+        <div class="col-md-8">
+          <h5 class="mb-0 text-black">DOWNLOAD RESUME PDF:</h5>
+            </div>
+              <div class="col-md-4 text-right">
+                <a href="#x" class="btn btn-outline-black m-1"><i class="md-icon dp14">file_download</i> <span>ENGLISH</span></a>
+            </div>
+        </div><!-- / row -->
+      </div><!-- / container-fluid -->
+  </div><!-- / cta -->
+
+
+
+</div>
 </template>
 
 <style>
 </style>
 
 <script>
-// var axios = require('axios');
+var axios = require('axios');
 
 export default {
   data: function() {
     return {
-      student: {
-        first: [],
-        last: [],
-        email: [],
-        phone: [],
-        bio: [],
-        link: [],
-        twitter: [],
-        website: [],
-        resume: [],
-        github: [],
-        photo: []
-      },
-      experience: {
-        start_date: [],
-        end_date: [],
-        job_title: [],
-        company_name: [],
-        details: []
-      },
-      education: {
-        start_date: [],
-        end_date: [],
-        degree: [],
-        university_name: [],
-        details: []
-      },
-      skills: {
-        skill_name: []
-      }
+      student: [],
+      message: "hello"
     };
   },
   created: function() {
     axios
-    .get("http://localhost:3000/api/students" + this.$route.params.id)
+    .get("/api/students/1")
     .then(response => {
       this.student = response.data;
     });
